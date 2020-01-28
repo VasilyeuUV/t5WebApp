3f6f950706b760fbdb755f7d65b1fa2973126506
Uladzislau Vasilyeu <vasilyeu.uv@mail.ru>
2020-01-28 13:41:16 +0300

    create project

---
+++ t5WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="t5WebApp.MvcApplication" Language="C#" %>
---
+++ t5WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(t5WebApp.Startup))]
+namespace t5WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
